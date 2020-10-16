933eb2f99cabe1285bb761f03d239ce6426ac4c8
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-10-15 20:32:55 -0700

    Refactor App and Bonuses components and add BonusItems

--- client/src/components/bonuses.jsx
+++ client/src/components/bonuses.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import BonusItem from './BonusItem.jsx';
 
-const Bonuses = (props) => {
-  let bonusTitle;
-  if (props.bonus && props.bonus.bonus_info.length === 1) {
-    bonusTitle = <h3>Bonus</h3>;
-  } else {
-    bonusTitle = <h3>Bonuses</h3>;
+class Bonuses extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bundleNumber: 1, //number
+      bonus: { bonus_info: [{ cover: '' }] }, //object
+      selectedSong: '', //string, representing url
+    };
+
+    this.selectSong = this.selectSong.bind(this);
   }
 
-  // const bonusItems = props.bonus.bonus_info;
+  componentDidMount() {
+    console.log('mounted')
+    $.get(`/bonus/${this.state.bundleNumber}`, (data) => {
+      console.log(data);
+      this.setState({ bonus: data });
+    });
+  }
 
-  return (
-    <div className='bonus-tier'>
-      <div className='bonus-title'>
-        {bonusTitle}
-      </div>
-      <div className='bonus-item-cover'>
-        <span>
-          <img src={props.bonus.cover} alt='Awesome Bonus Sountrack'></img>
-        </span>
+  selectSong() {
+    console.log('clicked');
+  }
 
+  render() {
+    let bonusTitle;
+    if (this.state.bonus && this.state.bonus.bonus_info.length === 1) {
+      bonusTitle = <h3>Bonus</h3>;
+    } else {
+      bonusTitle = <h3>Bonuses</h3>;
+    }
+
+    return (
+      <div className='bonus-tier'>
+        <div className='bonus-title'>
+          {bonusTitle}
+        </div>
+        <div>
+          <BonusItem bonus={this.state.bonus} selectSong={this.selectSong} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
+  // const bonusItems = props.bonus.bonus_info;
 export default Bonuses;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,40 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 import Bonuses from './components/bonuses.jsx';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bundleNumber: 1, //number
-      bonus: { bonus_info: [{ cover: '' }] }, //object
-      selectedSong: '', //string, representing url
-    };
-
-    this.selectSong = this.selectSong.bind(this);
-  }
-  componentDidMount() {
-    console.log('mounted')
-    $.get(`/bonus/${this.state.bundleNumber}`, (data) => {
-      console.log(data);
-      this.setState({ bonus: data });
-    });
-  }
-
-  selectSong() {
-    console.log('clicked');
-  }
-
-  render() {
-
-    return (
-      <div>
-        <Bonuses bonus={this.state.bonus} selectSong={this.selectSong} />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Bonuses />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('bonuses'));
