22b99dcf8e1cda037a5837cd8ee2c8063a65fea7
[Kimarie Haverty-Bozman] <[KimarieHavertyBozman@Gmail.com]>
2020-10-16 15:58:09 -0700

    Remove uneccessary App component and directly render Bonuses

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,13 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Bonuses from './components/bonuses.jsx';
 
-
-const App = () => {
-  return (
-    <div>
-      <Bonuses />
-    </div>
-  );
-}
-
 ReactDOM.render(<App />, document.getElementById('bonuses'));
